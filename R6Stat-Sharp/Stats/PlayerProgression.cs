38c934cfa0c8012eaf668aa1891a93177ce9b00f
Cenk <cenk.ergen1@gmail.com>
2021-02-25 15:11:56 +0300

    added documentation

--- R6Stat-Sharp/Stats/PlayerProgression.cs
+++ R6Stat-Sharp/Stats/PlayerProgression.cs
@@ -10,12 +10,21 @@ namespace R6Stats.Stats
     /// </remarks>
     public class PlayerProgression
     {
+        /// <summary>
+        /// Clearance level of the player
+        /// </summary>
         [JsonProperty("level")]
         public int Level { get; private set; }
 
+        /// <summary>
+        /// Alpha pack drop probability
+        /// </summary>
         [JsonProperty("lootbox_probability")]
         public double LootboxProb { get; private set; }
 
+        /// <summary>
+        /// Total xp of the player
+        /// </summary>
         [JsonProperty("total_xp")]
         public int TotalXP { get; private set; }
     }
