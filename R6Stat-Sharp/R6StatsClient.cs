806b9435daa99718d55d7d9658dd13338d60c34c
Cenk <cenk.ergen1@gmail.com>
2021-02-25 15:53:26 +0300

    general cleanup

--- R6Stat-Sharp/R6StatsClient.cs
+++ R6Stat-Sharp/R6StatsClient.cs
@@ -3,7 +3,6 @@ using R6Stats.Enums;
 using R6Stats.Models;
 using R6Stats.Response;
 using R6Stats.Response.Interfaces;
-using R6Stats.Stats.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
@@ -18,19 +17,20 @@ namespace R6Stats
     /// <summary>
     /// API client for parsing Rainbow6 Siege Game Data
     /// </summary>
-    public class R6StatsClient
+    public sealed class R6StatsClient : IDisposable
     {
         private const string _baseUrl = "https://api2.r6stats.com/public-api/";
 
         private readonly HttpClient _httpClient;
         private readonly ImmutableDictionary<string, IEnumerable<string>> _headers;
+        private readonly JsonSerializerSettings _defaultSerializerSettings;
 
         private readonly string _key;
 
         /// <summary>
         /// Create an instance of <see cref="R6StatsClient"/>
         /// </summary>
-        /// <param name="config"><see cref="Config"> containing the necessary information to instanciate the client</param>
+        /// <param name="config"><see cref="Config"/> containing the necessary information to instanciate the client </param>
         public R6StatsClient ( Config config )
         {
             _key = config.ApiKey;
@@ -44,12 +44,17 @@ namespace R6Stats
             };
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _key);
             _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            _defaultSerializerSettings = new JsonSerializerSettings()
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            };
         }
 
         /// <summary>
         /// Create an instance of <see cref="R6StatsClient"/>
         /// </summary>
-        /// <param name="config"><see cref="Config">Object containing the necessary information to instanciate the client</param>
+        /// <param name="config"><see cref="Config"/>Object containing the necessary information to instanciate the client</param>
         /// <param name="headers">Optional request headers to be used while making REST API requests</param>
         public R6StatsClient ( Config config, IDictionary<string, IEnumerable<string>> headers ) : this(config)
         {
@@ -75,6 +80,10 @@ namespace R6Stats
         /// <param name="username">Username the stats belongs to</param>
         /// <param name="platform">Platform the user plays on</param>
         /// <returns><see cref="APIResponse{T}"/> containing the request status data and a <see cref="SeasonalResponse"/> with the requested data</returns>
+        /// <example>
+        /// How to get seasonal stats
+        /// <c>var seasonal = await GetSeasonalStats("username", Platform.PC)</c>
+        /// </example>
         public async Task<APIResponse<SeasonalResponse>> GetSeasonalStats ( string username, Platform platform ) =>
             await GetResponse<SeasonalResponse>(Endpoints.GetSeasonal(username, platform));
 
@@ -114,23 +123,7 @@ namespace R6Stats
         public async Task<APIResponse<Leaderboard>> GetLeaderboard ( Platform platform, Region region = Region.ALL ) =>
             await GetResponse<Leaderboard>(Endpoints.GetLeaderboard(platform, region));
 
-        /*
-        public async Task<IReadOnlyDictionary<Platform, GenericResponse>> PerformLookup ( string username, LookupSettings settings )
-        {
-            IDictionary<Platform, GenericResponse> platforms = new Dictionary<Platform, GenericResponse>();
-
-            foreach (var platform in Enum.GetNames(typeof(Platform)).Cast<Platform>())
-            {
-                var platformResponse = await GetResponse<GenericResponse>(Endpoints.GetGeneric(username, platform));
-
-                if (platformResponse.Error != null)
-                    continue;
-            }
-
-            return new ReadOnlyDictionary<Platform, GenericResponse>(platforms);
-        }
-        */
-        private async Task<APIResponse<T>> GetResponse<T> ( string endpoint, JsonSerializerSettings settings = default ) where T : IPayload
+        private async Task<APIResponse<T>> GetResponse<T> ( string endpoint, JsonSerializerSettings settings = null ) where T : class, IPayload
         {
             var response = await _httpClient.GetAsync(endpoint);
 
@@ -142,9 +135,18 @@ namespace R6Stats
                 var error = JsonConvert.DeserializeObject<ApiError>(content);
                 return new APIResponse<T>(error.Status, error.Error);
             }
-            var payload = JsonConvert.DeserializeObject<T>(content, settings);
+            var payload = JsonConvert.DeserializeObject<T>(content, settings ?? _defaultSerializerSettings);
+            response.Dispose();
 
             return new APIResponse<T>(response.StatusCode.ToString(), payload);
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public void Dispose ( )
+        {
+            _httpClient.Dispose();
         }
     }
 }
