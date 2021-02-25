bec4e58c099b5b012a38f19b6412387c7b9a5c8a
Cenk <cenk.ergen1@gmail.com>
2021-02-25 13:13:07 +0300

    added documentation and qol upgrades

--- R6Stat-Sharp/Response/Leaderboard.cs
+++ R6Stat-Sharp/Response/Leaderboard.cs
@@ -12,7 +12,7 @@ namespace R6Stats.Response
     /// </summary>
     public sealed class Leaderboard : IReadOnlyList<LeaderboardSlot>, IReadOnlyCollection<LeaderboardSlot>, IEnumerable<LeaderboardSlot>, IPayload
     {
-        private List<LeaderboardSlot> _leaderboard;
+        private readonly List<LeaderboardSlot> _leaderboard;
 
         internal Leaderboard ( )
         {
@@ -25,10 +25,50 @@ namespace R6Stats.Response
             _leaderboard = leaderboard.ToList();
         }
 
+        /// <summary>
+        /// Get user info for a leaderboard index
+        /// </summary>
+        /// <param name="index">An index starting from 0</param>
+        /// <returns>User info for the user in the specified index</returns>
         public LeaderboardSlot this[int index] => _leaderboard[index];
 
+        /// <summary>
+        /// Get user info for a username from a leaderboard
+        /// </summary>
+        /// <param name="username">Username to be parsed</param>
+        /// <returns>User info for the specified username, if not found returns the <see langword="null"/>/></returns>
+        public LeaderboardSlot this[string username] { get
+            {
+                if (TryGetPlayer(username, out var leaderboardSlot))
+                    return leaderboardSlot;
+                else
+                    return null;
+            } }
+
+        /// <summary>
+        /// Count of players in the learderboard (should always be 100)
+        /// </summary>
         public int Count => _leaderboard.Count;
 
+        /// <summary>
+        /// Get the top 10 player of the leaderboard
+        /// </summary>
+        public IEnumerable<LeaderboardSlot> Top10 => _leaderboard.GetRange(0, 10);
+
+        public bool TryGetPlayer(string username, out LeaderboardSlot player )
+        {
+            player = _leaderboard.Find(x => string.Equals(x.Username, username, System.StringComparison.OrdinalIgnoreCase));
+            if (player != default)
+                return true;
+            else
+                return false;
+
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <returns></returns>
         public IEnumerator<LeaderboardSlot> GetEnumerator ( ) => _leaderboard.GetEnumerator();
 
         IEnumerator IEnumerable.GetEnumerator ( ) => this.GetEnumerator();
