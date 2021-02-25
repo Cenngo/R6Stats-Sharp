f0148b3d2352d35fa899c0948155e3e0c36269e4
Cenk <cenk.ergen1@gmail.com>
2021-02-25 15:12:11 +0300

    added documentation

--- R6Stat-Sharp/Stats/GenericResponseStats.cs
+++ R6Stat-Sharp/Stats/GenericResponseStats.cs
@@ -2,7 +2,9 @@
 
 namespace R6Stats.Stats
 {
-
+    /// <summary>
+    /// Represents the stats returned in a <see cref="R6Stats.Response.GenericResponse"/>
+    /// </summary>
     public class GenericResponseStats
     {
         [JsonProperty("general")]
@@ -15,26 +17,50 @@ namespace R6Stats.Stats
         public GenericGamemodeStats Gamemode { get; private set; }
     }
 
+    /// <summary>
+    /// Object contatining the overall game stats for game queues like "Ranked", "Casual" ...
+    /// </summary>
     public class GenericQueueStats
     {
+        /// <summary>
+        /// Stats for casual queue
+        /// </summary>
         [JsonProperty("casual")]
         public QueueStat Casual { get; private set; }
 
+        /// <summary>
+        /// Stats for ranked queue
+        /// </summary>
         [JsonProperty("ranked")]
         public QueueStat Ranked { get; private set; }
 
+        /// <summary>
+        /// Stats for queues other than  ranked and casual
+        /// </summary>
         [JsonProperty("other")]
         public QueueStat Other { get; private set; }
     }
 
+    /// <summary>
+    /// Object contating the game stats for gamemodes like "Bomb", "Hostage", "Secure Area"
+    /// </summary>
     public class GenericGamemodeStats
     {
+        /// <summary>
+        /// Stats for the gamemode "Bomb"
+        /// </summary>
         [JsonProperty("bomb")]
         public BombStats Bomb { get; private set; }
 
+        /// <summary>
+        /// Stats for the gamemode "Secure Area"
+        /// </summary>
         [JsonProperty("secure_area")]
         public SecureAreaStats SecureArea { get; private set; }
 
+        /// <summary>
+        /// Stats for the gamemode "Hostage"
+        /// </summary>
         [JsonProperty("hostage")]
         public HostageStats Hostage { get; private set; }
     }
