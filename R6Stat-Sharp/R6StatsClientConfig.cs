ba8751a506ee7171340854de3e66b9b9ce2c059b
Cenk <cenk.ergen1@gmail.com>
2021-02-25 13:13:26 +0300

    sealed the class

--- R6Stat-Sharp/R6StatsClientConfig.cs
+++ R6Stat-Sharp/R6StatsClientConfig.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Config object used to initialize the API client <see cref="R6StatsClient"/>
     /// </summary>
-    public class R6StatsClientConfig
+    public sealed class R6StatsClientConfig
     {
         /// <summary>
         /// Bearer token for the API which can be obtained from <see href="https://discord.gg/pUdraS3">R6Stats Discord Server</see>
